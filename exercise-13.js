1f2f508352741fc0d75f05ec868b6f8c29cffb9e
Faip Mardoni <faipmardoni@gmail.com>
2018-02-28 23:58:27 +0700

    exercise week 2 done

---
+++ exercise-13.js
@@ -0,0 +1,26 @@
+function xo(str) {
+    // you can only write your code here!
+    var x='';
+    var o='';
+    
+    for(i=0; i<str.length; i++) {
+        if(str[i]=='x') {
+          x++
+        } else {
+          o++
+        }
+    }
+
+    if(x==o) {
+        return true;
+    }else {
+        return false;
+    }
+  }
+  
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
