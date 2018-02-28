e8ae0ad63dee982ea770641a0931d26767fe72d4
Faip Mardoni <faipmardoni@gmail.com>
2018-02-28 15:16:58 +0700

    exercise 6 udah kelar

---
+++ exercise-6.js
@@ -0,0 +1,66 @@
+//1. Melakukan Looping Menggunakan While
+var i = 0;
+
+console.log('LOOPING PERTAMA');
+while (i<=18) {
+    i=i+2
+    console.log(i,'- I love coding');    
+}
+var j=20;
+
+console.log('LOOPING KEDUA');
+while (j>=1) {
+    console.log(j,'- I will become fullstack developper');
+    j=j-2;
+}
+
+console.log('\n');
+
+//2. Melakukan Looping Menggunakan For
+console.log('LOOPING PERTAMA');
+
+for (var k=1; k<=20; k++) {
+    console.log(k,' - I love coding');
+}
+
+console.log('LOOPING KEDUA');
+
+for (var l=20; l>=1; l--) {
+    console.log(l,' - I will become fullstack developper');
+}
+
+console.log('\n');
+
+//3. Angka Ganjil dan Genap
+
+for (var m=1; m<=100; m++) {
+    if (m % 2 === 0) {
+        console.log('GENAP');
+    } else {
+        console.log('GANJIL');
+    }
+}
+
+for  (var n=1; n<=100; n=n+2) {
+    if (n%3==0) {
+        console.log(n,'KELIPATAN 3');
+    } else {
+        console.log('\"\"');
+    }
+}
+
+for (var o=1; o<=100; o=o+5) {
+    if(o%6==0) {
+        console.log(o,'KELIPATAN 6');
+    } else {
+        console.log('\"\"');
+    }
+}
+
+for (var p=1; p<=100; p=p+9) {
+    if(p%10==0) {
+        console.log(p,'KELIPATAN 10');
+    } else {
+        console.log('\"\"');
+    }
+}
